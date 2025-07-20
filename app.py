9238e1023103dbdc34f136ae4d38e33a8ea82d23
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-07-20 16:27:40 -0500

    Update app.py
    Removed timeout

--- app.py
+++ app.py
@@ -5,11 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    global timeout_occurred
-    if timeout_occurred:
-        # Reset the timeout flag when accessing the home page
-        timeout_occurred = False
-    
     return render_template('chat.html')
 
 # handle to send chat requests
