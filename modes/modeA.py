dc779a1a82fb683aeec6820d1c33c989d7ae6898
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-07-21 11:05:12 -0500

    Update modeA.py
    Store dataframe

--- modes/modeA.py
+++ modes/modeA.py
@@ -1,2 +1,11 @@
-def modeA(file):
+import pandas as pd
+
+def modeA(exte, file):
     print(file.filename)
+
+    if exte == 'csv':
+        df = pd.read_csv(file)
+    elif exte == 'xlsx':
+        df = pd.read_excel(file)
+
+    print(df)
