66a298f91ee34b02aed9dec8a1d78e271f1afe0b
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-07-21 11:11:10 -0500

    Update modeA.py

--- modes/modeA.py
+++ modes/modeA.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 def modeA(exte, file):
-    print(file.filename)
-
     if exte == 'csv':
         df = pd.read_csv(file)
     elif exte == 'xlsx':
         df = pd.read_excel(file)
 
-    print(df)
+    from functions.periods import extract_periods
+
+    extract_periods(df)
