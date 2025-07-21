6465d6e7c44159e64f7ee0af10a0ef6ba4107a41
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-07-21 08:40:00 -0500

    Update extension.py
    Added allowed files

--- functions/extension.py
+++ functions/extension.py
@@ -1,2 +1,6 @@
+ALLOWED_EXTENSIONS = {'csv', 'xls', 'xlsx'}
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 def extension():
   pass
