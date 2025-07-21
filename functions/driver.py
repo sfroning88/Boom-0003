a8ebde2012024f387d67081858d2941a61eeba6c
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-07-21 10:58:39 -0500

    Update driver.py
    Added modeA functionality

--- functions/driver.py
+++ functions/driver.py
@@ -1,6 +1,8 @@
 def process_driver(exte):
     from functions.extension import ALLOWED_EXTENSIONS
     if exte in ALLOWED_EXTENSIONS[:3]:
+        from modes.modeA import modeA
         print('Mode = A')
+        modeA(file)
     else:
         pass
