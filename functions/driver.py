112278a6cd0ddb018b310fcb8e756727cac11741
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-07-21 11:04:50 -0500

    Update driver.py
    Updated requirements

--- functions/driver.py
+++ functions/driver.py
@@ -1,8 +1,8 @@
-def process_driver(exte):
+def process_driver(exte, file):
     from functions.extension import ALLOWED_EXTENSIONS
     if exte in ALLOWED_EXTENSIONS[:3]:
         from modes.modeA import modeA
         print('Mode = A')
-        modeA(file)
+        modeA(exte, file)
     else:
         pass
