d9b2f8b99eeba9c573d1cbde24ec3874310571fb
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-07-22 09:11:39 -0500

    Update driver.py
    Removed debug statement

--- functions/driver.py
+++ functions/driver.py
@@ -2,7 +2,6 @@ def process_driver(exte, file):
     from functions.extension import ALLOWED_EXTENSIONS
     if exte in ALLOWED_EXTENSIONS[:3]:
         from modes.modeA import modeA
-        print('Mode = A')
         modeA(exte, file)
     else:
         pass
